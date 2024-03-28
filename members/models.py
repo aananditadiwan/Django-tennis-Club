a6b5e7ca7f2246035f2277fb14f5ae0ec2504cbe
Aanandita Diwan <aananditadiwan@gmail.com>
2024-03-28 15:29:03 +0530

    database operations on model using shell

--- members/models.py
+++ members/models.py
@@ -1,3 +1,8 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    phone = models.IntegerField(null=True)
+    join_date = models.DateField(null=True)
